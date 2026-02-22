85395f54a32180198034c41bee100d2e2be4d2c7
chris <chris@LAPTOPCHRIS>
2026-02-22 12:35:29 -0600

    menu1

--- EjeRepo3/Program.cs
+++ EjeRepo3/Program.cs
@@ -1,1 +1,9 @@
 ﻿//Programa con Repositorio 3
+Console.WriteLine("Ingrese el tipo de cliente \n" +
+    "1)Estudiante \n" +
+    "2)Docente \n" +
+    "3)Administrativo \n" +
+    "4)Externo_");
+int opcion = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el monto a pagar_");
+double monto = double.Parse(Console.ReadLine());
