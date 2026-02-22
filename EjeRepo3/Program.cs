88897d3d61bc783c8911e2bdf90ffa13899eb83e
chris <chris@LAPTOPCHRIS>
2026-02-22 13:04:50 -0600

    switch1

--- EjeRepo3/Program.cs
+++ EjeRepo3/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Programa con Repositorio 3
+double descuento = 0;
+double descuentoCupon = 0;
 Console.WriteLine("Ingrese el tipo de cliente \n" +
     "1)Estudiante \n" +
     "2)Docente \n" +
@@ -14,3 +16,29 @@ Console.WriteLine("seleccione el metodo de pago \n" +
 int pago = int.Parse(Console.ReadLine());
 Console.WriteLine("Posee cupon? (s/n)_");
 string cupon = Console.ReadLine();
+if(monto > 0 && cupon == "s" || cupon == "n")
+{
+    switch (opcion)
+    {
+        case 1:
+            switch (pago)
+            {
+                case 1:
+                    descuento = monto * 0.1;
+                    break;
+                case 2:
+                    descuento = monto * 0.07;
+                    break;
+                case 3:
+                    if(monto > 200)
+                    {
+                        descuento = monto * 0.05;
+                    }break;
+                default: Console.WriteLine("Opcion invalida en el metodo de pago"); break;
+            }
+            break;
+    }
+}else
+{
+    Console.WriteLine("Valor invalido, revise sus respuestas en monto y cupon");
+}
